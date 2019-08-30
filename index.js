14b3ad9e6c682f8e377cd88a1d3cce8e002f9728
Castor Köhler <castor.kohler@futurice.com>
2019-08-30 16:03:01 +0300

    Refactor and add /help endpoint

--- index.js
+++ index.js
@@ -1,23 +1,66 @@
 const TelegramBot = require('node-telegram-bot-api');
 require('dotenv').config()
 
+function bookingToString(booking) {
+  var bookingStr = `*${booking.start_time} - ${booking.end_time}*\n_${booking.booker}_\n${booking.description}`;
+  return bookingStr;
+}
+
 // replace the value below with the Telegram token you receive from @BotFather
 const token = process.env.TELEGRAM_API_TOKEN;
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
 
-bot.onText(/\/motesrummet/, (msg) => {
+const fakeData = [
+  {
+    'booker': 'John Doe',
+    'start_time': '12:30',
+    'end_time': '13:00',
+    'description': 'A very important but short meeting'
+  },
+  {
+    'booker': 'Jane Doe',
+    'start_time': '14:00',
+    'end_time': '17:00',
+    'description': 'Brainstorming for where to eat'
+  },
+  {
+    'booker': 'Julius Caesar',
+    'start_time': '17:00',
+    'end_time': '18:00',
+    'description': 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque pen'
+  },
+  {
+    'booker': 'Cookiemonster',
+    'start_time': '20:00',
+    'end_time': '23:59',
+    'description': 'Coookiiiiiiies'
+  }
+]
+
+bot.onText(/\/help/, (msg) => {
+  const helpMsg = "\
+Hello, I'm a bot for easy access to FARS booking system. These are my availble commands:\n\n\
+_help_\nDisplays this message\n\n\
+_bookable_\nDisplays all available bookables\n\n\
+_[bookable]_\nDisplays the bookings for the bookable today\n\n\
+_[bookable] [YYYY-MM-DD]_\nDisplays all bookings for the given date for the given bookable"
+
+  bot.sendMessage(msg.chat.id, helpMsg, {"parse_mode": "Markdown"});
+});
+
+bot.onText(/\/((?!help).+)/, (msg, params) => {
   const chatId = msg.chat.id;
   const date = new Date(msg.date*1000);
-  const bookings = [
-    '*12:30 - 13:00*\n_Castor Köhler_\nNågo jätteviktigt möte',
-    '*14:00 - 17:00*\n_TF-IC (Thomas Langenskiöld)_\nStörsta giffelfeasten någonsin',
-    '*17:00 - 18:00*\n_Julius Caesar_\nLorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque pen',
-    '*20:00 - 23:59*\n_Cooooookiieeee_\nÄta kex'
-  ];
+  const bookable = params[1];
 
-  const resp_msg = `*The bookings for today, ${date.toDateString()}, are:*\n———————————————\n${bookings.join("\n\n")}`;
+  // Make query from FARS api for bookings of bookable today
+  const bookings = fakeData;
+
+  const bookingsStr = bookings.map( b => bookingToString(b) ).join("\n\n");
+
+  const resp_msg = `*The bookings for today, ${date.toDateString()}, are:*\n———————————————\n${bookingsStr}`;
   bot.sendMessage(chatId, resp_msg, {"parse_mode":"Markdown"});
 });
 
